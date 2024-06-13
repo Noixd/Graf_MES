be10c9fd6799dbadae97315c5c368d0a5174e1a7
Noix <noiodox@mail.ru>
2024-06-13 08:38:18 +0300

    Collumn menu/Delete func Update func - weird querry

--- Graf_MES/MainWindow.xaml.cs
+++ Graf_MES/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Data;
 
 
 
+
 namespace Graf_MES
 {
     /// <summary>
@@ -25,14 +26,14 @@ namespace Graf_MES
     public partial class MainWindow : Window
     {
         private static string DB = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = Graf_DB.accdb";
-        private OleDbConnection connection = new OleDbConnection(DB);
+        //private OleDbConnection connection = new OleDbConnection(DB);
 
         public MainWindow()
         {
             InitializeComponent();
             OpenGrafTable();
             OpenWorkPositionsTable();
-            
+
         }
 
         public void InitComboBox1()
@@ -194,19 +195,24 @@ namespace Graf_MES
             connection.Close();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        public void Refresh_func()
         {
-
+            InitializeComponent();
+            OpenGrafTable();
+            OpenWorkPositionsTable();
+            InitComboBox1();
+            InitComboBox2();
         }
+
         private void comboBox1_Initialized(object sender, EventArgs e)
         {
             InitComboBox1();
         }
         private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch(comboBox1.SelectedIndex)
+            switch (comboBox1.SelectedIndex)
             {
-                case 0: 
+                case 0:
                     OpenCrew1Table();
                     break;
                 case 1:
@@ -238,11 +244,8 @@ namespace Graf_MES
 
         private void MenuItemRefresh_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
-            OpenGrafTable();
-            OpenWorkPositionsTable();
-            InitComboBox1();
-            InitComboBox2();
+            Refresh_func();
+
         }
 
 
@@ -256,6 +259,152 @@ namespace Graf_MES
             Close();
         }
 
+        private void Delete_button_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void ContextMenu_delete_row_DG1_Click(object sender, RoutedEventArgs e)
+        {
+            int id_to_delete;
+            TextBlock text_to_delete;
+            string querry = null;
+
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this data?", "Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                if (dataGrid1.SelectedItem != null)
+                {
+                    id_to_delete = int.Parse(dataGrid1.SelectedIndex.ToString());
+                    text_to_delete = dataGrid1.Columns[0].GetCellContent(dataGrid1.Items[id_to_delete]) as TextBlock;
+                    id_to_delete = int.Parse(text_to_delete.Text);
+                    //MessageBox.Show(id_to_delete.ToString());
+
+                    OleDbConnection connection = new OleDbConnection(DB);
+
+                    switch (comboBox1.SelectedIndex)
+                    {
+
+                        case 0:
+                            querry = "DELETE FROM `sta` WHERE `Код` =";
+                            break;
+
+                        case 1:
+                            querry = "DELETE FROM `management_staff` WHERE `Код` =";
+                            break;
+
+                        case 2:
+                            querry = "DELETE FROM `management_staff` WHERE `Код` =";
+                            break;
+                    }
+
+                    OleDbCommand command = new OleDbCommand(querry + id_to_delete, connection);
+
+                    connection.Open();
+
+                    if (command.ExecuteNonQuery() == 1) MessageBox.Show("Data deleted successfully", "Delete");
+
+                    else MessageBox.Show("Error", "Delete");
+
+                    connection.Close();
+
+                    dataGrid4.UnselectAll();
+
+                    Refresh_func();
+                }
+            }
+        }
+
+        private void ContextMenu_delete_row_DG4_Click(object sender, RoutedEventArgs e)
+        {
+            int id_to_delete;
+            TextBlock text_to_delete;
+            string querry = null;
+            
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this data?", "Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                if (dataGrid4.SelectedItem != null)
+                {
+                    id_to_delete = int.Parse(dataGrid4.SelectedIndex.ToString());
+                    text_to_delete = dataGrid4.Columns[0].GetCellContent(dataGrid4.Items[id_to_delete]) as TextBlock;
+                    id_to_delete = int.Parse(text_to_delete.Text);
+                    //MessageBox.Show(id_to_delete.ToString());
+
+                    OleDbConnection connection = new OleDbConnection(DB);
+
+                    switch (comboBox2.SelectedIndex)
+                    {
+
+                        case 0:
+                            querry = "DELETE FROM `staff` WHERE `Код` = ";
+                            break;
+
+                        case 1:
+                            querry = "DELETE FROM `management_staff` WHERE `Код` = ";
+                            break;
+                    }
+
+                    OleDbCommand command = new OleDbCommand(querry + id_to_delete, connection);
+
+                    connection.Open();
+
+                    if (command.ExecuteNonQuery() == 1) MessageBox.Show("Data deleted successfully", "Delete");
+
+                    else MessageBox.Show("Error", "Delete");
+
+                    connection.Close();
+
+                    dataGrid4.UnselectAll();
+
+                    Refresh_func();
+                }
+            }
+        }
+
+        private void dataGrid4_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int edit_row;
+            int edit_column;
+            TextBlock text_to_edit;
+            int id_to_edit;
+            string querry;
+
+            MessageBox.Show(e.EditAction.ToString());
+            if (e.EditAction.ToString() == "Commit") {
+
+                edit_row = int.Parse(e.Row.AlternationIndex.ToString());
+                edit_column = int.Parse(e.Column.DisplayIndex.ToString());
+                var edit_value = ((TextBox)e.EditingElement).Text.ToString();
+                text_to_edit = dataGrid4.Columns[0].GetCellContent(dataGrid4.Items[edit_row]) as TextBlock;
+                id_to_edit = int.Parse(text_to_edit.Text);
+
+
+                querry = "UPDATE 'work_positions' SET " + dataGrid4.Columns[edit_column].Header.ToString() + " = '"+ edit_value + "' WHERE 'Код' = " + id_to_edit + "";
+                MessageBox.Show(querry);
+
+                OleDbConnection connection = new OleDbConnection(DB);
+                OleDbCommand command = new OleDbCommand(querry, connection);
+
+                connection.Open();
+
+                try
+                {
+                    if (command.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("Данные изменены", "Изменение");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Данные не изменены", "Изменение");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                connection.Close();
+            }
+        }
     }
 }
